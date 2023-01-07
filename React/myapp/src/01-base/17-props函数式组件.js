9b06c7c187be7f5c9c3ebf04adc0587d8be3315e
guhuan769 <769540542@qq.com>
2023-01-07 13:56:04 +0800

    我爱你

---
+++ React/myapp/src/01-base/17-props函数式组件.js
@@ -0,0 +1,20 @@
+/*
+函数性属性天生就支持属性写法
+*/
+
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* 类组件 1*/}
+        <Navbar title="导航"></Navbar>
+        {/* 函数式组件 */}
+        <Sidebar bg="yellow"  ></Sidebar>
+      </div>
+    )
+  }
+}
