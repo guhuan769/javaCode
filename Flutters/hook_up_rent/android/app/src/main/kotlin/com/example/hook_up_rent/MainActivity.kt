c45b90e5994a010dda152e9935e1d7b3b5eb8488
guhuan769 <769540542@qq.com>
2023-04-30 17:01:44 +0800

    新增APP版本更新

--- Flutters/hook_up_rent/android/app/src/main/kotlin/com/example/hook_up_rent/MainActivity.kt
+++ Flutters/hook_up_rent/android/app/src/main/kotlin/com/example/hook_up_rent/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.hook_up_rent
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
 }
--- Flutters/hook_up_rent/lib/main.dart
+++ Flutters/hook_up_rent/lib/main.dart
@@ -2,7 +2,7 @@
  * @Author: guhuan769 769540542@qq.com
  * @Date: 2023-04-16 14:32:56
  * @LastEditors: guhuan769 769540542@qq.com
- * @LastEditTime: 2023-04-29 14:56:28
+ * @LastEditTime: 2023-04-30 10:34:47
  * @FilePath: \hook_up_rent\lib\main.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
